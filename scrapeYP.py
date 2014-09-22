bd378508a16621565b46a612ad16553d29c164d8
System Administrator <root@pashas-mac-mini.home>
2014-09-22 17:39:39 -0400

    with Tor

--- scrapeYP.py
+++ scrapeYP.py
@@ -1,31 +1,68 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException
 from bs4 import BeautifulSoup
-import time, re, pymysql, html5lib, random, sys, urlparse, urllib, ConfigParser
+import time, re, pymysql, html5lib, random, sys, urlparse, urllib, ConfigParser, subprocess, signal, os
 
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
+tor_path = config.get('tor', 'binary')
 chrome_path = config.get('chromedriver', 'path')
 conn = pymysql.connect(host=config.get('database', 'host'), user=config.get('database', 'user'), passwd=config.get('database', 'passwd'), db=config.get('database', 'db'))
 offset = 0
 commit = True
 
+
+def tor_start():
+	subprocess.call([tor_path])
+	time.sleep(4)
+
+def tor_quit():
+	p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+	out, err = p.communicate()
+	
+	for line in out.splitlines():
+		if 'TorBrowser' in line:
+			pid = int(line.split(None, 1)[0])
+			print 'quit pid', pid
+			os.kill(pid, signal.SIGKILL)
+
+	time.sleep(4)
+
+
 def scrape(id, url, biz_id):
-	driver = webdriver.Chrome(chrome_path)
+	fprofile = FirefoxProfile()
+	fprofile.set_preference('network.proxy.type', 1)
+	fprofile.set_preference('network.proxy.socks', "127.0.0.1")
+	fprofile.set_preference('network.proxy.socks_port', 9150)
+
+	driver = webdriver.Firefox(firefox_profile=fprofile)
+
+	#driver = webdriver.Chrome(chrome_path)
 	driver.implicitly_wait(3)
 	driver.get(url)
 	time.sleep(1+random.random()*3)
 	
 
 	#print 'url', driver.current_url
+	
+	if '403 Forbidden' in driver.title:
+		print 'we are busted, get a new IP'
+		driver.quit()
+		tor_quit()
+		tor_start()
+		return
 
-	if(driver.current_url != url):
+
+	if(not '/mip/' in driver.current_url):
 		print 'forward'
 		driver.quit()
+		update_link(id)
 		return
+
 
 	
 	prog = re.compile('(mailto:)([\w.]+@[\w]+\.+[\w.]+)', re.I)				
@@ -80,6 +117,7 @@ def get_links():
 	print 'offset:', offset
 	
 	max = 10000
+	tor_start()
 	while i < max:
 	
 		query = 'SELECT link.id, link.link, link.biz_id FROM link, email WHERE link.is_scraped=0 AND link_type_id=2 AND link.biz_id=email.id AND email.status_id=-1 AND email.web = "" LIMIT 100'
